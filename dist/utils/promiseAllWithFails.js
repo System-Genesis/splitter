26ee2fe15a841c796b606840f18d8977dea91f2a
tommyafek <61648986+tommyafek@users.noreply.github.com>
2021-06-06 13:29:48 +0300

    Delete dist directory

--- dist/utils/promiseAllWithFails.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function promiseAllWithFails(promiseArray, valueErr) {
-    return await Promise.all(promiseArray.map(promise => promise.catch(err => (valueErr ? valueErr : err))));
-}
-exports.default = promiseAllWithFails;
-;
-//# sourceMappingURL=promiseAllWithFails.js.map
